8b337f860d40c49608f0b722aeaa9d1a11f3b91e
Ashu Shekhar <ashu.p25@medhaviskillsuniversity.edu.in>
2026-09-17 01:17:21 +0530

    Fix tyre walk-forward v2 float assertion

--- tests/test_audit_race_strategy_tyre_model_walkforward_v2.py
+++ tests/test_audit_race_strategy_tyre_model_walkforward_v2.py
@@ -1,11 +1,13 @@
+import pytest
+
 from audit_race_strategy_tyre_model_walkforward_v2 import _ols, _pearson
 
 
 def test_ols_tolerates_float_rounding():
     fit = _ols([1.0, 2.0, 3.0], [0.0, 0.1, 0.2])
     assert fit is not None
-    assert fit[0] == 0.1
-    assert fit[1] == -0.1
+    assert fit[0] == pytest.approx(0.1)
+    assert fit[1] == pytest.approx(-0.1)
 
 
 def test_pearson_detects_perfect_linear_shape():
