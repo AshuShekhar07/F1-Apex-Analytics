d5a981c7158e4cf82c6f73a8e58d08141feb4c07
Ashu Shekhar <ashu.p25@medhaviskillsuniversity.edu.in>
2026-09-17 00:55:02 +0530

    Test corrected tyre walk-forward validation

---
+++ tests/test_audit_race_strategy_tyre_model_walkforward_v2.py
@@ -0,0 +1,13 @@
+from audit_race_strategy_tyre_model_walkforward_v2 import _ols, _pearson
+
+
+def test_ols_tolerates_float_rounding():
+    fit = _ols([1.0, 2.0, 3.0], [0.0, 0.1, 0.2])
+    assert fit is not None
+    assert fit[0] == 0.1
+    assert fit[1] == -0.1
+
+
+def test_pearson_detects_perfect_linear_shape():
+    assert _pearson([0.0, 0.1, 0.2], [0.0, 1.0, 2.0]) is not None
+    assert _pearson([0.0, 0.1, 0.2], [0.0, 1.0, 2.0]) > 0.999
